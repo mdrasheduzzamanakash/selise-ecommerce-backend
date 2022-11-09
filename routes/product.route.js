27bb4a11f6d4b20d364c933b654b3c8e6b9fd46c
Md. Rasheduzzaman <mdrasheduzzamanakash@gmail.com>
2022-11-10 02:14:48 +0600

    Add files via upload
    add readInChunk api

--- routes/product.route.js
+++ routes/product.route.js
@@ -14,6 +14,19 @@ productRoute.route('/').get((req, res, next) => {
     });
 });
 
+// get product in chunk
+productRoute.route('/chunk/:pageIndex/:pageSize').get((req, res, next) => {
+    Product.find({}, (error, data) => {
+        const { pageIndex } = req.params;
+        const pageSize = req.params.pageSize - 1;
+        if (error) {
+            return next(error);
+        }
+        const chunkData = data.slice(pageIndex * pageSize, (pageIndex + 1) * pageSize + 1);
+        res.json(chunkData);
+    });
+});
+
 // add a single product
 productRoute.route('/add-Product').post((req, res, next) => {
     Product.create(req.body, (error, data) => {
